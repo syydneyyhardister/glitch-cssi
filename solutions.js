89e131f8b72a03b91cc7a25eafd429db378b680d
syydneyyhardister <41082236+syydneyyhardister@users.noreply.github.com>
2020-07-31 11:46:53 -0700

    cssi-day-1

---
+++ solutions.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  // Brush settings
+  noFill()
+  strokeWeight(5)
+
+  // Ring 1: Blue
+  stroke(10, 134, 205)
+  ellipse(50, 50, 50)
+
+  // Ring 2: Yellow
+  stroke(255, 214, 0)
+  ellipse(80, 80, 50)
+
+  // Ring 3: Black
+  stroke(0, 0, 0)
+  ellipse(110, 50, 50)
+
+  // Ring 4: Green
+  stroke(33, 176, 76)
+  ellipse(140, 80, 50)
+
+  // Ring 5:
+  stroke(234, 30, 35)
+  ellipse(170, 50, 50)
+}
