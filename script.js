7155b96884f5a2c02ecb9558fb096d295a920f64
syydneyyhardister <41082236+syydneyyhardister@users.noreply.github.com>
2020-07-31 11:54:09 -0700

    cssi-day-1-practice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function setup() {
   // Code here runs only once
-  createCanvas(800, 600);
+  createCanvas(1350, 800);
 }
 
 function draw() {
@@ -10,20 +10,84 @@ function draw() {
   background(220);
 
   noFill();
-  strokeWeight(5);
+  strokeWeight(60);
+  //red ring
+  stroke(255,0,0);
+  ellipse(365, 360, 400, 400);
+  //white ring
+  stroke(255,255,255);
+  ellipse(365, 360, 330, 330);
+  //red ring
+  stroke(255,0,0);
+  ellipse(365, 360, 270, 270);
   //blue ring
-  stroke(0,133,199);
-  ellipse(50, 50, 50, 50);
-  //yellow ring
-  stroke(244,195,0);
-  ellipse(80, 80, 50, 50);
-  //blue ring
-  stroke(0,0,0);
-  ellipse(110, 50, 50, 50);
-  //blue ring
-  stroke(0,159,61);
-  ellipse(140, 80, 50, 50);
-  //blue ring
-  stroke(223,0,36);
-  ellipse(170, 50, 50, 50);
+  fill(0,0,255);
+  stroke(0,0,255);
+  ellipse(365, 360, 210, 210);
+  //star
+  star();  
+  
+  
+  //windows logo
+  windows();
+  
+}
+function star(){
+  strokeWeight(10);
+  stroke(255);
+  fill(255,255,255);
+  beginShape();
+  vertex(480,330);
+  vertex(400,330);
+  vertex(365,245);
+  vertex(330,330);
+  vertex(250,330);
+  vertex(315,385);
+  vertex(290,455);
+  vertex(365,415);
+  vertex(440,455);
+  vertex(415,385);
+  endShape(CLOSE);
+}
+
+function windows(){
+  strokeWeight(10);
+  stroke(246,83,20);
+  fill(246,83,20);
+  beginShape();
+  vertex(650,250);
+  vertex(650,100);
+  vertex(850,80);
+  vertex(850,230);
+  endShape(CLOSE);
+  
+  strokeWeight(10);
+  stroke(124,187,0);
+  fill(124,187,0);
+  beginShape();
+  vertex(880,75);
+  vertex(880,225);
+  vertex(1080,205);
+  vertex(1080,55);
+  endShape(CLOSE);
+  
+  strokeWeight(10);
+  stroke(0,161,241);
+  fill(0,161,241);
+  beginShape();
+  vertex(650,280);
+  vertex(650,430);
+  vertex(850,410);
+  vertex(850,260);
+  endShape(CLOSE);
+  
+  strokeWeight(10);
+  stroke(255,187,0);
+  fill(255,187,0);
+  beginShape();
+  vertex(880,255);
+  vertex(880,405);
+  vertex(1080,385);
+  vertex(1080,235);
+  endShape(CLOSE);
 }
